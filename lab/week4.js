024c346c26ba5f8acff71ed0c2822a7da6b723b4
Dan Peacock <dan.peacock@kellogg.northwestern.edu>
2021-01-28 20:03:11 -0600

    Added some comments

--- lab/week4.js
+++ lab/week4.js
@@ -17,30 +17,37 @@ async function pageLoaded() {
   let json = await response.json()
 
   // writes the returned JSON to the console
-  console.log(json)
-  let products = json.products
+  // console.log(json)
   // 🔥 start here: write code to loop through the products; each
   // iteration of the loop should call your renderProduct function
   // make it work first; then extract to the separate renderProduct
   // function after it's 100% working without it
   
+  // Grab array of interest from the JSON object
+  let products = json.products
+  
+  // This finds the 'products' class in the HTML and inserts
+  // properly formatted HTML with classes to render the argued product
   function renderProduct(productData) {
     let outputElement = document.querySelector('.products')
+    // Can this be simplified?
     outputElement.insertAdjacentHTML('beforeend', `
     <div class="p-4 w-full md:w-1/2 lg:w-1/3">
-    <div class="border h-full p-4 flex flex-col">
-      <h2 class="text-lg font-bold mb-4">${productData.name}</h2>
-      <div class="mb-4"><img src="${productData.image}">
-      </div>
-      <div class="mb-4 text-gray-900">
-        ${productData.description}
-      </div>
-      <div class="mt-auto text-purple-500 text-2xl">${productData.price}</div>
+      <div class="border h-full p-4 flex flex-col">
+        <h2 class="text-lg font-bold mb-4">${productData.name}</h2>
+        <div class="mb-4"><img src="${productData.image}">
+        </div>
+        <div class="mb-4 text-gray-900">
+          ${productData.description}
+        </div>
+        <div class="mt-auto text-purple-500 text-2xl">${productData.price}
+        </div>
       </div>
     </div>
     `)
   }
 
+  //Iterate over all products and render them
   for (let i = 0; i < products.length; i++) {
     product = products[i]
     console.log(product.name)
